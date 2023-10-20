ce02182a6eec14a8d9baee663d2eaa3001bf91aa
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-10-19 23:19:09 -0300

    Revert "Iniciar o projeto"
    This reverts commit d6417d92f95d21164dcff5f523f66b024510e65f.

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-
-Console.WriteLine("************************");
-Console.WriteLine(mensagemDeBoasVindas);
-Console.WriteLine("************************");
