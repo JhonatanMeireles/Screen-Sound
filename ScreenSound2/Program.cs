5a9cca6dd7bb16e61bf0ec56cabdd4e81b39f9ce
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-10-25 23:01:41 -0300

    Limitando acesso na classe utilizando o private

--- ScreenSound2/Program.cs
+++ ScreenSound2/Program.cs
@@ -3,14 +3,16 @@ Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
 musica1.duracao = 277;
-musica1.disponivel = true;
+musica1.EscreveDisponivel(true);
+Console.WriteLine(musica1.LeDisponivel);
 
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
-musica1.duracao = 367;
-musica1.disponivel = false;
+musica2.duracao = 367;
+musica2.EscreveDisponivel(false);
+
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
