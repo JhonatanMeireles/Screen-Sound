ae7b7a860b5263a4efa1459fd02a018ff25c2963
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-10-30 20:53:24 -0300

    Incluindo o método get e set para alterar o modo de acesso aos valores da propriedade

--- ScreenSound2/Musica.cs
+++ ScreenSound2/Musica.cs
@@ -1,25 +1,23 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    private bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public String DesricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}.";
 
-    public void EscreveDisponivel(bool value) 
-    {
-        disponivel = value;
-    }
 
-    public bool LeDisponivel() 
-    { 
-        return disponivel;
-    }
+    public int a { get; set; }
+    public int b { get; set;  }
+
+    public int Somar => a + b;
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
         }
@@ -30,7 +28,7 @@
     }
     public void ExibirFichaResumida()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"artista: {artista}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"artista: {Artista}");
     }
 }
--- ScreenSound2/Program.cs
+++ ScreenSound2/Program.cs
@@ -1,17 +1,20 @@
 ﻿
 Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 277;
-musica1.EscreveDisponivel(true);
-Console.WriteLine(musica1.LeDisponivel);
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 277;
+musica1.Disponivel = true;
+musica1.a = 1;
+musica1.b = 2;
+Console.WriteLine(musica1.DesricaoResumida);
+Console.WriteLine(musica1.Somar);
 
 
 Musica musica2 = new Musica();
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
-musica2.EscreveDisponivel(false);
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel= false;
 
 
 musica1.ExibirFichaTecnica();
